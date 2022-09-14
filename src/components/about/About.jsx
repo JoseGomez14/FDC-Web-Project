6698ace879b6811f7bc1ff3c49f64397d196fa7e
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-14 17:41:58 -0500

    Complete "about"

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,13 +23,32 @@ const About = ({idSpecie, setPopUpState}) => {
 
             {docState === 'load' && <h1>Cargando...</h1>}
             {docState === 'empty' && <h1>La especie no se ha encontrado, encuentra otras especies <NavLink to={'/gallery'}>aquí</NavLink></h1>}
-            {docState === 'exists' &&
+            {docState === 'exists' ?
                 <article>
-                    <h1>{specie.commonName}</h1>
-                    {specie.sound !== ''?
-                    <audio src={specie.sound} controls></audio>
-                    :<h2>Se ha encontrado la especie, lastimosamente a un no tenemos registro de su maravilloso sonido :(</h2>}
+                    <h1 title='Nombre comùn'>{specie.commonName}</h1>
+                    <p title='Nombre científico'>{specie.scientificName}</p>
+                    {specie.images.map((image, index)=>{
+                        return <figure key={'image-' + index}>
+                            <img src={image} alt={specie.commonName} width='400px'></img>
+                        </figure>
+                    })}
+                    {specie.sound !== '' && <div>
+                        <h3>Sonido de la especie</h3>
+                        <audio src={specie.sound} controls></audio>
+                        {specie.srcSound && <p><b>Sonido tomado de: </b>{specie.srcSound}</p>}
+                    </div>}
+                    {specie.description  && <p><b>Descripción: </b>{specie.description}</p>}
+                    {specie.genus        && <p><b>Género: </b>{specie.genus}</p>}
+                    <p><b>Clase: </b>{specie.className}</p>
+                    <p><b>Hábitat: </b>{specie.habitat}</p>
+                    {specie.size !== 0     && <p><b>Tamaño: </b>{specie.size} cm</p>}
+                    {specie.food           && <p><b>Alimentos: </b>{specie.food}</p>}
+                    {specie.skinColor      && <p><b>Color del pelaje: </b>{specie.skinColor}</p>}
+                    {specie.state === true && <p><b>¡Especie en vía de extinción!</b></p>}
+                    {specie.mapUrl         && <iframe src={specie.mapUrl} width="600" height="450" allowfullscreen="" loading="lazy" title='Ubicación del registro de la especie'></iframe>}
+                    {specie.inaturalistUrl && <p><b>Observación en iNaturalist: </b><a href={specie.inaturalistUrl} target='_blank' rel='noreferrer'>{specie.inaturalistUrl}</a></p>}
                 </article>
+                :<h2>Se ha encontrado la especie, lastimosamente a un no tenemos registro de su maravilloso sonido :(</h2>
             }
         </section>
     );
