83ae79d625d5aa8c20bace5df2253e16967a6882
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-21 19:39:41 -0500

    Add theme with design colors

---
+++ src/theme.jsx
@@ -0,0 +1,10 @@
+const theme = {
+    veryLightGray: '#F4F4F4',
+    darkGray: '#737373',
+    darkCyan: '#00A181',
+    softGreen: '#A4E473',
+    veryDarkCyan: '#004651',
+    mostlyBlack: '#00272D',
+}
+ 
+export default theme;
