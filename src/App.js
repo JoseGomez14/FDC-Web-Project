02a52bc0af342d9ae17d494f89d7dc5d29232959
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-21 22:02:15 -0500

    Implement components of information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+import AboutUs from './components/main/AboutUs';
+import Allies from './components/main/Allies';
+import Objetives from './components/main/Objectives';
+import Productions from './components/main/Productions';
+import History from './components/main/History';
+import Contacts from './components/main/Contacts';
+import Home from './components/main/Home';
 
 /**
  * Componente encargado de mostrar la información de página principal
@@ -9,10 +16,19 @@ import './App.css';
 function App() {
   return (
     <main className="App">
-      <h1>Página Principal</h1>
-      <h2>Contenido del grupo</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <Home id='home'/>
+      
+      <AboutUs id='about-us'/>
+
+      <Productions id='productions'/>
+
+      <Objetives id='objectives'/>
+
+      <Allies id='allies'/>
+
+      <History id='history'/>
+
+      <Contacts id='contact'/>
     </main>
   );
 }
