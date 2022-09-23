bff7683eec2b454a0dc8622ccc5bf883e023ceb9
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-23 00:11:42 -0500

    Implements elements of style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,58 +1,88 @@
 import React from 'react'
-import {useParams, NavLink} from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container, ContainerColumn, ContainerSlider } from '../../elements/Containers';
+import { H2 } from '../../elements/Titles'
+import Paragraph from '../../elements/Paragraph'
 import useGetSpecie from '../../hooks/useGetSpecie';
+import { BackButton } from '../../elements/Buttons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 /**
- * Componente que permite mostrar a profundidad la información de una especie, es decir,
- * muestra todos los datos y registros multimedia de la especie.
- * 
- * Funciona como componente en "gallery" si se pasan los parámetros y como ruta dinámica
- * si se consulta desde el navegador con un id de especie válido
+ * Componente que permite mostrar a profundidad la información de una especie, es decir,
+ * muestra todos los datos y registros multimedia de la especie.
+ * 
+ * Funciona como componente en "gallery" si se pasan los parámetros y como ruta dinámica
+ * si se consulta desde el navegador con un id de especie válido
  * 
  * @param {string} idSpecie identificador de la especie búscada
  * @param {*} setPopUpState función que permite modificar el estado de ventana
  * @returns Retorna la información de la especie consultada
  */
-const About = ({idSpecie, setPopUpState}) => {
-    const {id} = useParams();
+const About = ({ idSpecie, setPopUpState }) => {
+    const { id } = useParams();
     const [specie, docState] = useGetSpecie(idSpecie || id);
-    
-    return (
-        <section>
-            {setPopUpState && <button onClick={() => setPopUpState(false)}>Volver a la galleria</button>}
+    const urlSpecie = document.location.origin + '/about/' + (idSpecie || id);
 
-            {docState === 'load' && <h1>Cargando...</h1>}
-            {docState === 'empty' && <h1>La especie no se ha encontrado, encuentra otras especies <NavLink to={'/gallery'}>aquí</NavLink></h1>}
-            {docState === 'exists' ?
-                <article>
-                    <h1 title='Nombre comùn'>{specie.commonName}</h1>
-                    <p title='Nombre científico'>{specie.scientificName}</p>
-                    {specie.images.map((image, index)=>{
-                        return <figure key={'image-' + index}>
-                            <img src={image} alt={specie.commonName} width='400px'></img>
-                        </figure>
-                    })}
-                    {specie.sound !== '' && <div>
-                        <h3>Sonido de la especie</h3>
-                        <audio src={specie.sound} controls></audio>
-                        {specie.srcSound && <p><b>Sonido tomado de: </b>{specie.srcSound}</p>}
-                    </div>}
-                    {specie.description  && <p><b>Descripción: </b>{specie.description}</p>}
-                    {specie.genus        && <p><b>Género: </b>{specie.genus}</p>}
-                    <p><b>Clase: </b>{specie.className}</p>
-                    <p><b>Reino: </b>{specie.kingdom}</p>
-                    <p><b>Hábitat: </b>{specie.habitat}</p>
-                    {specie.size !== 0     && <p><b>Tamaño: </b>{specie.size} cm</p>}
-                    {specie.food           && <p><b>Alimentos: </b>{specie.food}</p>}
-                    {specie.skinColor      && <p><b>Color del pelaje: </b>{specie.skinColor}</p>}
-                    {specie.state === true && <p><b>¡Especie en vía de extinción!</b></p>}
-                    {specie.mapUrl         && <iframe src={specie.mapUrl} width="600" height="450" allowfullscreen="" loading="lazy" title='Ubicación del registro de la especie'></iframe>}
-                    {specie.inaturalistUrl && <p><b>Observación en iNaturalist: </b><a href={specie.inaturalistUrl} target='_blank' rel='noreferrer'>{specie.inaturalistUrl}</a></p>}
-                </article>
-                :<h2>Se ha encontrado la especie, lastimosamente a un no tenemos registro de su maravilloso sonido :(</h2>
-            }
-        </section>
+    return (
+        <Container>
+            {setPopUpState && <BackButton onClick={() => setPopUpState(false)} title='Volver a la gallería'><FontAwesomeIcon icon={faArrowLeft} /></BackButton>}
+            <ContainerColumn>
+
+                {docState === 'load' && <h1>Cargando...</h1>}
+                {docState === 'empty' && <h1>La especie no se ha encontrado, encuentra otras especies <NavLink to={'/gallery'}>aquí</NavLink></h1>}
+                {docState === 'exists' &&
+                    <Article>
+                        <H2 title='Nombre común'>{specie.commonName}</H2>
+
+                        <ContainerSlider>
+                            {specie.images.map((image, index) => {
+                                return <img src={image} alt={specie.commonName} width='400px' key={'image-' + index}></img>
+                            })}
+                        </ContainerSlider>
+
+                        {specie.sound !== '' && <ContainerColumn>
+                            <Paragraph><b>Sonido de la especie</b></Paragraph>
+                            <audio src={specie.sound} controls w></audio>
+                            {specie.srcSound && <Paragraph><b>Sonido tomado de: </b>{specie.srcSound}</Paragraph>}
+                        </ContainerColumn>}
+
+                        <Paragraph><b>Nombre científico: </b><em>{specie.scientificName}</em></Paragraph>
+
+                        {specie.description && <Paragraph><b>Descripción: </b>{specie.description}</Paragraph>}
+
+                        {specie.genus && <Paragraph><b>Género: </b>{specie.genus}</Paragraph>}
+
+                        <Paragraph><b>Clase: </b>{specie.className}</Paragraph>
+                        <Paragraph><b>Reino: </b>{specie.kingdom}</Paragraph>
+                        <Paragraph><b>Hábitat: </b>{specie.habitat}</Paragraph>
+
+                        {specie.size !== 0 && <Paragraph><b>Tamaño: </b>{specie.size} cm</Paragraph>}
+
+                        {specie.food && <Paragraph><b>Alimentos: </b>{specie.food}</Paragraph>}
+
+                        {specie.skinColor && <Paragraph><b>Color del pelaje: </b>{specie.skinColor}</Paragraph>}
+
+                        {specie.state === true && <Paragraph style={{ color: '#ee5513' }}><b>¡Especie en vía de extinción!</b></Paragraph>}
+
+                        {specie.mapUrl && <iframe src={specie.mapUrl} width="600" height="450" allowfullscreen="" loading="lazy" title='Ubicación del registro de la especie'></iframe>}
+
+                        {specie.inaturalistUrl && <Paragraph><b>Observación en iNaturalist: </b><a href={specie.inaturalistUrl} target='_blank' rel='noreferrer'>{specie.inaturalistUrl}</a></Paragraph>}
+
+                        <Paragraph><b>Compartir especie: </b>{urlSpecie}</Paragraph>
+                    </Article>
+                }
+            </ContainerColumn>
+        </Container>
     );
 }
- 
+
+const Article = styled.article`
+    margin-top: 20%;
+    p{
+        margin: 10px;
+    }
+`
+
 export default About;
