c29ff829a00c84350b91894a6a5e69049d94969b
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-23 00:12:09 -0500

    Implements elements of style

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
-import useGetSpecies from '../../hooks/useGetSpecies'
-import ItemGallery from './ItemGallery'
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import useGetSpecies from '../../hooks/useGetSpecies';
+import ItemGallery from './ItemGallery';
 import Search from './Search';
+import { Container, ContainerColumn, ContainerGallery } from '../../elements/Containers';
+import { H2 } from '../../elements/Titles';
 
 /**
  * Muestra todas las especies disponibles, además integra una barra de búsqueda por nombre
@@ -18,51 +20,54 @@ const Gallery = () => {
 
     useEffect(() => {
         if (species.length > 0 || empty) {
-          setLoadState(false)
+            setLoadState(false)
         }
-      }, [empty, species])
+    }, [empty, species])
 
     return (
-        <section>
-            <h1>Galleria</h1>
+        <Container style={{paddingTop: '5rem'}}>
+            <ContainerColumn style={{alignItems: 'start'}}>
+                <H2>Galleria</H2>
+                {loadState &&
+                    <h2>Cargando...</h2>
+                }
 
-            {loadState &&
-                <h2>Cargando...</h2>
-            }
-
-            {!loadState && !empty &&
-                <Search
-                    setSearchState={setSearchState}
-                    setFilterSpecies={setFilterSpecies}
-                    species={species}
-                />
-            }
-
-            {!searchState?
-                species.map((specie, index) => {
-                    return <ItemGallery key={'specie-main' + index}
-                        specie={specie}
+                {!loadState && !empty &&
+                    <Search
+                        setSearchState={setSearchState}
+                        setFilterSpecies={setFilterSpecies}
+                        species={species}
                     />
-                })
-                :
-                filterSpecies.map((specie, index) => {
-                    return <ItemGallery key={'specie-main' + index}
-                        specie={specie}
-                    />
-                })
-            }
+                }
 
-            {searchState && filterSpecies.length === 0 &&
-                <h2>¡Vaya! No hay especies registradas con las características seleccionadas.</h2>
-            }
+                <ContainerGallery>
+                    {!searchState ?
+                        species.map((specie, index) => {
+                            return <ItemGallery key={'specie-main' + index}
+                                specie={specie}
+                            />
+                        })
+                        :
+                        filterSpecies.map((specie, index) => {
+                            return <ItemGallery key={'specie-main' + index}
+                                specie={specie}
+                            />
+                        })
+                    }
+                </ContainerGallery>
 
-            {empty &&
-                <article>
-                    <h2>¡Vaya! No hay especies registradas por el momento.</h2>
-                    <p>Pero no es impedimento para que conozcas más acerca de <em>Fuera de Contexto</em> <NavLink to={'/'}>!aquí¡</NavLink></p>
-                </article>
-            }
-        </section>
+                {searchState && filterSpecies.length === 0 &&
+                    <h2>¡Vaya! No hay especies registradas con las características seleccionadas.</h2>
+                }
+
+                {empty &&
+                    <article>
+                        <h2>¡Vaya! No hay especies registradas por el momento.</h2>
+                        <p>Pero puedes conocer más acerca de <em>Fuera de Contexto</em> <NavLink to={'/'}>!aquí¡</NavLink></p>
+                    </article>
+                }
+            </ContainerColumn>
+        </Container>
     );
 }
 
--- src/components/gallery/ItemGallery.jsx
+++ src/components/gallery/ItemGallery.jsx
@@ -1,5 +1,8 @@
-import React,  { useState } from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { GridItemGallery, ContainerFloat } from '../../elements/Containers';
 import IconClass from '../../elements/IconClass';
+import theme from '../../theme';
 import About from '../about/About';
 
 /**
@@ -8,32 +11,43 @@ import About from '../about/About';
  * @param {*} specie información de la especie a mostrar
  * @returns retorna el resumen de una especie en específico y la forma de ver más información de la misma
  */
-const ItemGallery = ({specie}) => {
+const ItemGallery = ({ specie }) => {
     const [popUpState, setPopUpState] = useState(false)
-    return ( 
-        <article>
+    return (
+        <GridItemGallery>
             <figure onClick={() => setPopUpState(true)}>
                 <img src={specie.images[0]} alt={specie.commonName} width='400px'></img>
                 <figcaption hidden>{specie.description}</figcaption>
+                <Article onClick={() => setPopUpState(true)} className='specieInfo'>
+                    <IconClass className={specie.className} height='50px' />
+                    <h1>{specie.commonName}</h1>
+                    <p><i>{specie.scientificName}</i></p>
+                </Article>
             </figure>
 
-            <h1>{specie.commonName}</h1>
-            <p><i>{specie.scientificName}</i></p>
-            
-            <IconClass className={specie.className} height='50px' fill='black'/>
-
-            <p><b>Hábitat: </b>{specie.habitat}</p>
-
-            <button onClick={() => setPopUpState(true)}>Acerca de {specie.commonName}</button>
-
             {popUpState &&
-                <About 
-                    idSpecie={specie.id}
-                    setPopUpState={setPopUpState}
-                />
+                <ContainerFloat>
+                    <About
+                        idSpecie={specie.id}
+                        setPopUpState={setPopUpState}
+                    />
+                </ContainerFloat>
             }
-        </article>
-     );
+        </GridItemGallery>
+    );
 }
- 
+
+const Article = styled.article`
+    padding-left: 1rem;
+    cursor: pointer;
+    color: ${theme.veryLightGray};
+    text-shadow: 1px 1px 2px ${theme.mostlyBlack};
+    h1{
+        font-size: 1rem;
+    }
+    p{
+        font-size: 0.9rem;
+    }
+`
+
 export default ItemGallery;
