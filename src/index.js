f84bfac1521d4037008ea4c6ab9735417a438121
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-10-08 23:37:03 -0500

    Add React Helmet and Page 404 to the routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
 import './index.css';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import App from './App';
@@ -9,31 +10,37 @@ import Footer from './elements/Footer';
 import Contacts from './components/main/Contacts';
 import Gallery from './components/gallery/Gallery';
 import About from './components/about/About';
+import Page404 from './components/main/Page404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={
-          <>
-            <App />
-          </>}
-        />
-        <Route path='/about/:id' element={
-          <>
-            <About />
-          </>}
-        />
-        <Route path='/gallery' element={
-          <>
-            <Gallery />
-          </>}
-        />
-      </Routes>
-      <Contacts />
-      <Footer />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={
+            <>
+              <App />
+            </>}
+          />
+          <Route path='/about/:id' element={
+            <>
+              <About />
+            </>}
+          />
+          <Route path='/gallery' element={
+            <>
+              <Gallery />
+            </>}
+          />
+          <Route path='/*' element={
+            <Page404 />
+          }></Route>
+        </Routes>
+        <Contacts />
+        <Footer />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
