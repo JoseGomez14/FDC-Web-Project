e50b5123a779590c742b3c1ae5e6baf17847902d
JoseG04 <jgcode04@gmail.com>
2022-10-10 14:36:34 -0500

    Update tags

--- src/elements/Slider.jsx
+++ src/elements/Slider.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Lazy } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/lazy';
 
 /**
  * Presentación de imagenes con controles y páginación
@@ -12,15 +13,19 @@ import 'swiper/css/pagination';
 const Slider = ({ slides }) => {
     return <>
         <Swiper
+            autoHeight={true}
             spaceBetween={10}
             pagination={{ clickable: true }}
             navigation={true}
-            modules={[Navigation, Pagination]}
+            lazy={true}
+            modules={[Navigation, Pagination, Lazy]}
             grabCursor={true}
         >
             {slides.map((item, index) => {
                 return <SwiperSlide key={"slide-" + index} width={500}>
-                    <img src={item} alt={"Fotografía"} />
+                    <div>
+                        <img src={item} alt={"Fotografía"} />
+                    </div>
                 </SwiperSlide>
             })}
         </Swiper>
